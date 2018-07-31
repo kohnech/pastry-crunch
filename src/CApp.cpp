cb4607a170ffe9b3a6b70fe56dd7302153a27896
Christian Valenzuela (local) <cvalenz@cvalenz-se-ll01.eu.trimblecorp.net>
2018-07-31 11:23:38 +0200

    Add event loop

--- src/CApp.cpp
+++ src/CApp.cpp
@@ -1,8 +1,45 @@
 #include "CApp.h"
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+
 CApp::CApp() {
+    Running = true;
 }
 
 int CApp::OnExecute() {
+    if(OnInit() == false) {
+        return -1;
+    }
+
+    SDL_Event Event;
+
+    while(Running) {
+        while(SDL_PollEvent(&Event)) {
+            OnEvent(&Event);
+        }
+
+        OnLoop();
+        OnRender();
+    }
+
+    OnCleanup();
+
     return 0;
+}
+
+bool CApp::OnInit() {
+    return true;
+}
+
+void CApp::OnEvent(SDL_Event* Event) {
+}
+
+void CApp::OnLoop() {
+}
+
+void CApp::OnRender() {
+}
+
+void CApp::OnCleanup() {
 }
