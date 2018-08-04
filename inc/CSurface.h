2972d1824dfb8a1e008ee68722a9b3b51ce84985
Christian Köhnenkamp <cvk5@me.com>
2018-08-04 14:07:52 +0200

    Add some comments to CSurface

--- inc/CSurface.h
+++ inc/CSurface.h
@@ -14,10 +14,35 @@ public:
 
     static SDL_Surface* loadSurface(std::string path);
 
+    /*!
+     * Load an image bmp to a surface
+     * @param File The image to load relative to exe or absolute path
+     * @return True if successful
+     */
     static SDL_Surface* OnLoad(const char* File);
 
+    /*!
+     * Draw an image on source
+     * @param Surf_Dest The destination surface
+     * @param Surf_Src The source surface
+     * @param x Destination x position
+     * @param y Destination y position
+     * @return True if successful
+     */
     static bool OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int x, int y);
 
+    /*!
+     * Draw a partial image on source
+     * @param Surf_Dest The destination surface
+     * @param Surf_Src The source surface
+     * @param x Destination x position
+     * @param y Destination y position
+     * @param x2 Source partial start x position
+     * @param y2 Source partial start y position
+     * @param w Width of source image
+     * @param h Height of source image
+     * @return True if successful
+     */
     static bool
     OnDraw(SDL_Surface* Surf_Dest, SDL_Surface* Surf_Src, int x, int y, int x2, int y2, int w, int h);
 
