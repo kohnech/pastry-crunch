fb547922a528d49648ec10d898a900e5f8cdb709
Christian Köhnenkamp <cvk5@me.com>
2018-08-06 22:43:45 +0200

    Add CMap

---
+++ inc/CMap.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SDL.h>
+#include <vector>
+
+#include "CTile.h"
+#include "CSurface.h"
+
+class CMap {
+public:
+    SDL_Surface* Surf_Tileset;
+
+private:
+    std::vector<CTile> TileList;
+    CSurface* mCSurface;
+
+public:
+    CMap();
+    ~CMap();
+
+public:
+    bool OnLoad(char* File);
+
+    void OnRender(SDL_Surface* Surf_Display, int MapX, int MapY);
+};
