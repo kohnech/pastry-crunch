f90501223403964c852eb9ce2697e285a8a8562f
Christian Köhnenkamp <cvk5@me.com>
2018-08-15 21:32:50 +0200

    Add sound assets

---
+++ inc/Sounds.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Assets.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <map>
+#include <string>
+
+class Sounds {
+public:
+    static Sounds           instance;
+
+    Sounds();
+
+    ~Sounds();
+
+    bool load(Assets& assets);
+
+    void cleanup();
+
+    void play(std::string ID);
+
+private:
+    std::map<std::string, Mix_Chunk*>     mSounds;
+};
+
